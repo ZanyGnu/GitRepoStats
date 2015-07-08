a74b5df63786387627ad610918af2f311a288bfb
ZanyGnu <ajaymartin@gmail.com>
2015-07-08 10:30:59 -0700

    add sample references to libgit2sharp

--- GitRepoStats/Program.cs
+++ GitRepoStats/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +11,30 @@ namespace GitRepoStats
     {
         public void Main(string[] args)
         {
-            Console.WriteLine("Hello world");            
+            Console.WriteLine("Hello world");
+
+            using (var repo = new Repository("C:\\rdnext\\Azure\\Compute"))
+            {
+                var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
+
+                foreach (Commit c in repo.Commits.Take(15))
+                {
+                    Console.WriteLine(string.Format("commit {0}", c.Id));
+
+                    if (c.Parents.Count() > 1)
+                    {
+                        Console.WriteLine("Merge: {0}",
+                            string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                    }
+
+                    Console.WriteLine(string.Format("Author: {0} <{1}>", c.Author.Name, c.Author.Email));
+                    Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
+                    Console.WriteLine();
+                    Console.WriteLine(c.Message);
+                    Console.WriteLine();
+                }
+            }
         }
+
     }
 }
