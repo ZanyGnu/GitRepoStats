beb419a9a334ac42d4c14d76d638e08464f3e2fa
ZanyGnu <ajaymartin@gmail.com>
2015-07-25 19:07:43 -0700

    introduce commit trend different from lines of code analyzer

--- RepoStats/Analyzers/CommitTrendAnalyzer.cs
+++ RepoStats/Analyzers/CommitTrendAnalyzer.cs
@@ -8,38 +8,34 @@ namespace RepoStats.Analyzers
 
     class CommitTrendAnalyzer : PatchAnalyzer
     {
-        Dictionary<DateTime, long> lineCountByDate = new Dictionary<DateTime, long>();
-        long totalLineCount = 0;
+        Dictionary<DateTime, long> commitCountByDate = new Dictionary<DateTime, long>();
+        long totalCommitCount = 0;
 
-        public void Visit(Commit c)
+        public void Visit(Commit commit)
         {
-            
+            DateTime commitDate = commit.Committer.When.DateTime.Round(TimeSpan.FromDays(1));
+            if (!commitCountByDate.ContainsKey(commitDate))
+            {
+                commitCountByDate[commitDate] = 0;
+            }
+            commitCountByDate[commitDate] += 1;
         }
 
         public void Visit(Commit commit, PatchEntryChanges patchEntryChanges)
         {
-            DateTime commitDate = commit.Committer.When.DateTime.Round(TimeSpan.FromDays(1));
-            if (!lineCountByDate.ContainsKey(commitDate))
-            {
-                lineCountByDate[commitDate] = 0;
-            }
-
-            lineCountByDate[commitDate] += patchEntryChanges.LinesAdded - patchEntryChanges.LinesDeleted;
-            totalLineCount += patchEntryChanges.LinesAdded - patchEntryChanges.LinesDeleted;
-
         }
 
         public void Write()
         {
-            Console.WriteLine("Line count by date");
-            IOrderedEnumerable<KeyValuePair<DateTime,long>> orderedLineCounts = lineCountByDate.OrderBy(c => c.Key);
+            Console.WriteLine("Commit count by date");
+            IOrderedEnumerable<KeyValuePair<DateTime,long>> orderedCommitCounts = commitCountByDate.OrderBy(c => c.Key);
             long currentTotalCount = 0;
-            foreach (KeyValuePair<DateTime, long> lineCountOnDate in orderedLineCounts)
+            foreach (KeyValuePair<DateTime, long> commitCountOnDate in orderedCommitCounts)
             {
-                currentTotalCount += lineCountOnDate.Value;
+                currentTotalCount += commitCountOnDate.Value;
                 Console.WriteLine("\t{0} |{1}", 
-                    lineCountOnDate.Key.ToString("dd/MM/yy"), 
-                    new String('*', (int)((currentTotalCount * 100)/ totalLineCount)));
+                    commitCountOnDate.Key.ToString("dd/MM/yy"), 
+                    new String('*', (int)((currentTotalCount * 100)/ totalCommitCount)));
             }
         }
 
---
+++ RepoStats/Analyzers/LinesOfCodeTrendAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoStats.Analyzers
+{
+    class LinesOfCodeTrendAnalyzer
+    {
+        Dictionary<DateTime, long> lineCountByDate = new Dictionary<DateTime, long>();
+        long totalLineCount = 0;
+
+        public void Visit(Commit commit)
+        {
+        }
+
+        public void Visit(Commit commit, PatchEntryChanges patchEntryChanges)
+        {
+            DateTime commitDate = commit.Committer.When.DateTime.Round(TimeSpan.FromDays(1));
+            if (!lineCountByDate.ContainsKey(commitDate))
+            {
+                lineCountByDate[commitDate] = 0;
+            }
+
+            lineCountByDate[commitDate] += patchEntryChanges.LinesAdded - patchEntryChanges.LinesDeleted;
+            totalLineCount += patchEntryChanges.LinesAdded - patchEntryChanges.LinesDeleted;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Line count by date");
+            IOrderedEnumerable<KeyValuePair<DateTime, long>> orderedLineCounts = lineCountByDate.OrderBy(c => c.Key);
+            long currentTotalCount = 0;
+            foreach (KeyValuePair<DateTime, long> lineCountOnDate in orderedLineCounts)
+            {
+                currentTotalCount += lineCountOnDate.Value;
+                Console.WriteLine("\t{0} |{1}",
+                    lineCountOnDate.Key.ToString("dd/MM/yy"),
+                    new String('*', (int)((currentTotalCount * 100) / totalLineCount)));
+            }
+        }
+
+        public string GetFormattedString()
+        {
+            return string.Empty;
+        }
+    }
+}
