e1772095da789adc5b49d1151caa3a183c9ec8de
ZanyGnu <ajaymartin@gmail.com>
2015-08-07 13:11:55 -0700

    remove dead code

--- RepoStats/Program.cs
+++ RepoStats/Program.cs
@@ -8,12 +8,9 @@ namespace RepoStats
     using System.Collections.Generic;
     using System.Configuration;
     using System.Diagnostics;
-    using System.Globalization;
-    using System.Linq;
 
     public class Program
-    {
-        
+    {        
         static void Main(string[] args)
         {
             string repoRoot = string.Empty;
@@ -31,28 +28,6 @@ namespace RepoStats
             
             using (var repo = new Repository(repoRoot))
             {
-                var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
-
-                int commitCount = repo.Commits.Count();
-                foreach (Commit c in repo.Commits.Take(15))
-                {
-                    Console.WriteLine(string.Format("commit {0}", c.Id));
-
-                    if (c.Parents.Count() > 1)
-                    {
-                        Console.WriteLine("Merge: {0}",
-                            string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
-                    }
-
-                    Console.WriteLine(string.Format("Author: {0} <{1}>", c.Author.Name, c.Author.Email));
-                    Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
-                    Console.WriteLine();
-                    Console.WriteLine(c.Message);
-                    Console.WriteLine();
-                }
-
-                //FindHotFilesAndCommitters(repoRoot, DateTime.Now.Subtract(TimeSpan.FromDays(30)), DateTime.Now);
-
                 List<FileChangeAnalyzer> patchAnalyzers = new List<FileChangeAnalyzer>();
 
                 patchAnalyzers.Add(new FileInfoAnalyzer(DateTime.Now.Subtract(TimeSpan.FromDays(30)), DateTime.Now));
