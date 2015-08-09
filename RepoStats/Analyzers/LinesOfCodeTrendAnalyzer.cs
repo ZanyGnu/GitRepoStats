9b70a1a150c30c39de745581fa02b10a661bd6fd
ZanyGnu <ajaymartin@gmail.com>
2015-08-09 00:28:59 -0700

    add graph to show trend of lines of code added by day

--- RepoStats/Analyzers/LinesOfCodeTrendAnalyzer.cs
+++ RepoStats/Analyzers/LinesOfCodeTrendAnalyzer.cs
@@ -6,6 +6,7 @@ namespace RepoStats.Analyzers
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
 
     class LinesOfCodeTrendAnalyzer: FileChangeAnalyzer
     {
@@ -24,8 +25,8 @@ namespace RepoStats.Analyzers
                 lineCountByDate[commitDate] = 0;
             }
 
-            lineCountByDate[commitDate] += fileChanges.LinesAdded - fileChanges.LinesDeleted;
-            totalLineCount += fileChanges.LinesAdded - fileChanges.LinesDeleted;
+            lineCountByDate[commitDate] += fileChanges.LinesAdded;
+            totalLineCount += fileChanges.LinesAdded ;
         }
 
         public void Write()
@@ -44,7 +45,42 @@ namespace RepoStats.Analyzers
 
         public string GetFormattedString()
         {
-            return string.Empty;
+            /*
+            categories: [{ 0}]
+                categories: [
+                'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+            ]
+            
+            // Series:
+            {
+                name: 'London',
+                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            },
+
+    */
+
+            StringBuilder categoryString = new StringBuilder();
+            StringBuilder seriesDataString = new StringBuilder();
+            IOrderedEnumerable<KeyValuePair<DateTime, long>> orderedLineCounts = lineCountByDate.OrderBy(c => c.Key);
+            long currentTotalCount = 0;
+            foreach (KeyValuePair<DateTime, long> lineCountOnDate in orderedLineCounts)
+            {
+                currentTotalCount += lineCountOnDate.Value;
+
+                categoryString.AppendFormat("'{0}', ", lineCountOnDate.Key.ToString("dd/MM/yy"));
+                seriesDataString.AppendFormat("{0}, ", currentTotalCount);
+            }
+
+            string seriesString = String.Format(
+                HtmlTemplates.Graph.SeriesTemplate, 
+                "Lines Of Code", 
+                seriesDataString.ToString());
+
+            return HtmlTemplates.Graph.ContainerTempalte + String.Format(
+                HtmlTemplates.Graph.GraphTemplate, 
+                categoryString.ToString(),
+                seriesString);
         }
     }
 }
--- RepoStats/HtmlTemplates.cs
+++ RepoStats/HtmlTemplates.cs
@@ -3,80 +3,80 @@ namespace RepoStats
 {
     public static class HtmlTemplates
     {
-        public static string HtmlPreTemplate = @"<html>
-
-          <html>
-
+        public static string HtmlPreTemplate = @"
+        <!DOCTYPE html>
+        <html>
             <head>
-            <meta http-equiv=Content-Type content='text/html; charset=windows-1252'>
-            <meta name=Generator content='Microsoft Word 15 (filtered)'>
-            <style>
-            .day {
-                pointer-events: all;
-            }
-            .day:hover { 
-                fill: red;
-            }
-            <!--
-             /* Font Definitions */
-             @font-face
-	            {font-family:'Cambria Math';
-	            panose-1:2 4 5 3 5 4 6 3 2 4;}
-            @font-face
-	            {font-family:'Calibri Light';
-	            panose-1:2 15 3 2 2 2 4 3 2 4;}
-            @font-face
-	            {font-family:Calibri;
-	            panose-1:2 15 5 2 2 2 4 3 2 4;}
-             /* Style Definitions */
-             p.MsoNormal, li.MsoNormal, div.MsoNormal
-	            {margin-top:0in;
-	            margin-right:0in;
-	            margin-bottom:8.0pt;
-	            margin-left:0in;
-	            line-height:107%;
-	            font-size:11.0pt;
-	            font-family:'Calibri',sans-serif;}
-            h1
-	            {mso-style-link:'Heading 1 Char';
-	            margin-top:12.0pt;
-	            margin-right:0in;
-	            margin-bottom:0in;
-	            margin-left:0in;
-	            margin-bottom:.0001pt;
-	            line-height:107%;
-	            page-break-after:avoid;
-	            font-size:16.0pt;
-	            font-family:'Calibri Light',sans-serif;
-	            color:#2E74B5;
-	            font-weight:normal;}
-            span.Heading1Char
-	            {mso-style-name:'Heading 1 Char';
-	            mso-style-link:'Heading 1';
-	            font-family:'Calibri Light',sans-serif;
-	            color:#2E74B5;}
-            .MsoChpDefault
-	            {font-family:'Calibri',sans-serif;}
-            .MsoPapDefault
-	            {margin-bottom:8.0pt;
-	            line-height:107%;}
-            @page WordSection1
-	            {size:8.5in 11.0in;
-	            margin:1.0in 1.0in 1.0in 1.0in;}
-            div.WordSection1
-	            {page:WordSection1;}
-            -->
-            </style>
+                <script type = 'text/javascript' src='http://code.jquery.com/jquery-1.9.1.js'></script>
+                <script type = 'text/javascript' src='http://code.highcharts.com/highcharts.js'></script>
+                <script type = 'text/javascript' src='http://code.highcharts.com/modules/exporting.js'></script>
+        
 
+                <style>
+                    .day {
+                        pointer-events: all;
+                    }
+                    .day:hover { 
+                        fill: red;
+                    }
+                    <!--
+                        /* Font Definitions */
+                        @font-face
+	                    {font-family:'Cambria Math';
+	                    panose-1:2 4 5 3 5 4 6 3 2 4;}
+                    @font-face
+	                    {font-family:'Calibri Light';
+	                    panose-1:2 15 3 2 2 2 4 3 2 4;}
+                    @font-face
+	                    {font-family:Calibri;
+	                    panose-1:2 15 5 2 2 2 4 3 2 4;}
+                        /* Style Definitions */
+                        p.MsoNormal, li.MsoNormal, div.MsoNormal
+	                    {margin-top:0in;
+	                    margin-right:0in;
+	                    margin-bottom:8.0pt;
+	                    margin-left:0in;
+	                    line-height:107%;
+	                    font-size:11.0pt;
+	                    font-family:'Calibri',sans-serif;}
+                    h1
+	                    {mso-style-link:'Heading 1 Char';
+	                    margin-top:12.0pt;
+	                    margin-right:0in;
+	                    margin-bottom:0in;
+	                    margin-left:0in;
+	                    margin-bottom:.0001pt;
+	                    line-height:107%;
+	                    page-break-after:avoid;
+	                    font-size:16.0pt;
+	                    font-family:'Calibri Light',sans-serif;
+	                    color:#2E74B5;
+	                    font-weight:normal;}
+                    span.Heading1Char
+	                    {mso-style-name:'Heading 1 Char';
+	                    mso-style-link:'Heading 1';
+	                    font-family:'Calibri Light',sans-serif;
+	                    color:#2E74B5;}
+                    .MsoChpDefault
+	                    {font-family:'Calibri',sans-serif;}
+                    .MsoPapDefault
+	                    {margin-bottom:8.0pt;
+	                    line-height:107%;}
+                    @page WordSection1        
+                        {size:8.5in 11.0in;
+                        margin:1.0in 1.0in 1.0in 1.0in; }
+                    div.WordSection1
+	                    {page:WordSection1;}
+                    -->
+                </style>
             </head>
+            <body>
             ";
 
         public static string HtmlPostTemplate = @"
             <br><br><hr><br>
             </div>
-
             </body>
-
             </html>";
 
         public static string FillerTemplate = @"
@@ -189,6 +189,70 @@ namespace RepoStats
                 </g>
             </svg>";
 
+        }
+
+        public static class Graph
+        {
+            public static string ContainerTempalte = @"<div id='container' style='min-width: 310px; height: 400px; margin: 0 auto'></div>";
+            public static string GraphTemplate = @"
+                <script type='text/javascript'>//<![CDATA[ 
+
+                $(function () {{
+                    $('#container').highcharts({{
+                        title: {{
+                            text: 'Daily code trend',
+                            x: -20 //center
+                        }},
+                        subtitle: {{
+                            text: '',
+                            x: -20
+                        }},
+                        xAxis: {{
+                            categories: [
+                                {0}
+                            ]
+                            /*
+                                categories: [
+                                'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+                            ]
+                            */
+                        }},
+                        yAxis: {{
+                            title: {{
+                                text: 'Count'
+                            }},
+                            plotLines: [{{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }}]
+                        }},
+                        /*tooltip: {{
+                            valueSuffix: '°C'
+                        }},*/
+                        legend: {{
+                            layout: 'vertical',
+                            align: 'right',
+                            verticalAlign: 'middle',
+                            borderWidth: 0
+                        }},
+                        series: [
+                        {1}
+                        ]
+                    }});
+                }});
+                //]]>  
+
+                </script>
+            ";
+
+            public static string SeriesTemplate = @"
+            {{
+                name: '{0}',
+                data: [{1}]
+            }}, 
+            ";
         }
     }
 }
