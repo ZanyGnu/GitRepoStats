919a7ff814423d6084d919018d7a231d4b26cf53
ZanyGnu <ajaymartin@gmail.com>
2015-08-16 16:57:42 -0700

    small format

--- RepoStats/HtmlTemplates.cs
+++ RepoStats/HtmlTemplates.cs
@@ -283,7 +283,9 @@ namespace RepoStats
               none;border-bottom:solid #BDD6EE 1.0pt;border-right:solid #BDD6EE 1.0pt;
               padding:0in 5.4pt 0in 5.4pt'>
               <p class=MsoNormal style='margin-bottom:0in;margin-bottom:.0001pt;line-height:
-              normal'>{3}</p>
+              normal'>
+                {3}
+              </p>
               </td>
              </tr>";
 
