06798c9d1d2c4af5bfff8a0d8033654c88e228d8
ZanyGnu <ajaymartin@gmail.com>
2015-08-25 18:30:47 -0700

    style the contrib table

--- RepoStats/HtmlTemplates.cs
+++ RepoStats/HtmlTemplates.cs
@@ -24,6 +24,10 @@ namespace RepoStats
                     }
                     text.month{font-size:10px;fill:#aaa}
                     text.wday{fill:#ccc;font-size:9px}
+                    td.contrib-title-box{background: #eee;vertical-align:top}
+                    .contrib-table {border: 1px solid #ccc; margin:0px; padding:0px;}
+                    .contrib-title-row {border: 1px solid #ccc; margin:0px; padding:0px;}
+                    .contrib-calendar-row td {border-top: 0px solid #ccc; margin:0px; padding:0px;}
                     <!--
                         /* Font Definitions */
                         @font-face
@@ -46,11 +50,7 @@ namespace RepoStats
 	                    font-family:'Calibri',sans-serif;}
                     h1
 	                    {mso-style-link:'Heading 1 Char';
-	                    margin-top:12.0pt;
-	                    margin-right:0in;
-	                    margin-bottom:0in;
-	                    margin-left:0in;
-	                    margin-bottom:.0001pt;
+	                    margin:7.0pt;
 	                    line-height:107%;
 	                    page-break-after:avoid;
 	                    font-size:16.0pt;
@@ -157,7 +157,6 @@ namespace RepoStats
         public static class SvgContribution
         {
             public static string SVGTemplatePre = @"
-            <h1>Contributions</h1><br>
             <style>
                 div.contribToolTip {
                     position: absolute;
@@ -172,87 +171,92 @@ namespace RepoStats
                     visibility: hidden;
                 }
             </style>
-            <div id='tooltipSpan' class='contribToolTip'>tool</div>
-            <svg width = '721' height='110' onload='init(evt)'>
-                <script type='text/ecmascript'><![CDATA[
-                    function cc(mouseEvt)
-                    {
-                        var svgObj = mouseEvt.target;
-                        var date = svgObj.getAttribute('data-date');
-                        var num = parseInt(svgObj.getAttribute('data-count'));
-                        if (num <= 0) {
-                            dataUrl = '';
-                        }
-                        else {
-                            dataUrl = 'commitsByDate/' + date + '.html';
-                        }
+            <table class='contrib-table'>
+                <tr class='contrib-title-row'><td class='contrib-title-box'><h1>Contributions</h1></td></tr>
+                <tr class='contrib-calendar-row'><td>
+                    <div id='tooltipSpan' class='contribToolTip'>tool</div>
+                    <svg width = '721' height='110' onload='init(evt)'>
+                        <script type='text/ecmascript'><![CDATA[
+                            function cc(mouseEvt)
+                            {
+                                var svgObj = mouseEvt.target;
+                                var date = svgObj.getAttribute('data-date');
+                                var num = parseInt(svgObj.getAttribute('data-count'));
+                                if (num <= 0) {
+                                    dataUrl = '';
+                                }
+                                else {
+                                    dataUrl = 'commitsByDate/' + date + '.html';
+                                }
 
-                        // reset the older cell value
-                        if (currentHighlightCell != null) { 
-                            currentHighlightCell.setAttribute('class', 'day');
-                        }
-                        // remember the current value
-                        currentHighlightCell = svgObj;
-                        // highlight the highlighted cell
-                        currentHighlightCell.setAttribute('class', 'selected');
+                                // reset the older cell value
+                                if (currentHighlightCell != null) { 
+                                    currentHighlightCell.setAttribute('class', 'day');
+                                }
+                                // remember the current value
+                                currentHighlightCell = svgObj;
+                                // highlight the highlighted cell
+                                currentHighlightCell.setAttribute('class', 'selected');
 
-                        $.ajax({    
-                            type: 'GET',
-                            url: dataUrl,             
-                            dataType: 'html',   //expect html to be returned                
-                            success: function(response)
-                            {                    
-                                document.getElementById('contributionsHolder').innerHTML = response;
+                                $.ajax({    
+                                    type: 'GET',
+                                    url: dataUrl,             
+                                    dataType: 'html',   //expect html to be returned                
+                                    success: function(response)
+                                    {                    
+                                        document.getElementById('contributionsHolder').innerHTML = response;
+                                    }
+                                });
                             }
-                        });
-                    }
 
-                    function init(evt) {                            
-                        contribToolTip = $('.contribToolTip');
-                        currentHighlightCell = null;
-                    }
+                            function init(evt) {                            
+                                contribToolTip = $('.contribToolTip');
+                                currentHighlightCell = null;
+                            }
 
-                    function ShowTooltip(evt) {        
-                        // Put tooltip in the right position, change the text and make it visible
-                        x = evt.clientX - 80 ;
-                        y = evt.clientY - 40;
-                        tooltipSpan.innerHTML = '<b>' + evt.target.getAttributeNS(null,'data-count') + ' contributions </b> on ' + evt.target.getAttributeNS(null,'data-date');                        
-                        contribToolTip.css({'top': y,'left': x, 'visibility':'visible'});
-                    }
+                            function ShowTooltip(evt) {        
+                                // Put tooltip in the right position, change the text and make it visible
+                                x = evt.clientX - 80 ;
+                                y = evt.clientY - 40;
+                                tooltipSpan.innerHTML = '<b>' + evt.target.getAttributeNS(null,'data-count') + ' contributions </b> on ' + evt.target.getAttributeNS(null,'data-date');                        
+                                contribToolTip.css({'top': y,'left': x, 'visibility':'visible'});
+                            }
 
-                    function HideTooltip(evt)
-                    {
-                        contribToolTip.css({visibility:'hidden'});
-                    }
-                ]]>
-                </script>
-                <g transform = 'translate(20, 20)' >";
+                            function HideTooltip(evt)
+                            {
+                                contribToolTip.css({visibility:'hidden'});
+                            }
+                        ]]>
+                        </script>
+                        <g transform = 'translate(20, 20)' >";
 
             public static string CellEntryTemplate = @"
-                    <g transform='translate({0}, 0)'><rect class='day' width='11' height='11' y='{1}' fill='{2}' data-count='{3}' data-date='{4}' onClick='cc(evt)'  onmousemove='ShowTooltip(evt)' onmouseout='HideTooltip(evt)'></rect></g>";
+                            <g transform='translate({0}, 0)'><rect class='day' width='11' height='11' y='{1}' fill='{2}' data-count='{3}' data-date='{4}' onClick='cc(evt)'  onmousemove='ShowTooltip(evt)' onmouseout='HideTooltip(evt)'></rect></g>";
 
             public static string SVGTemplatePost = @"
-                    <text x='26' y='-5' class='month'>Aug</text>
-                    <text x='91' y='-5' class='month'>Sep</text>
-                    <text x='143' y='-5' class='month'>Oct</text>
-                    <text x='195' y='-5' class='month'>Nov</text>
-                    <text x='260' y='-5' class='month'>Dec</text>
-                    <text x='312' y='-5' class='month'>Jan</text>
-                    <text x='364' y='-5' class='month'>Feb</text>
-                    <text x='416' y='-5' class='month'>Mar</text>
-                    <text x='481' y='-5' class='month'>Apr</text>
-                    <text x='533' y='-5' class='month'>May</text>
-                    <text x='598' y='-5' class='month'>Jun</text>
-                    <text x='650' y='-5' class='month'>Jul</text>
-                    <text text-anchor='middle' class='wday' dx='-10' dy='9' style='display: none;'>S</text>
-                    <text text-anchor='middle' class='wday' dx='-10' dy='22'>M</text>
-                    <text text-anchor='middle' class='wday' dx='-10' dy='35' style='display: none;'>T</text>
-                    <text text-anchor='middle' class='wday' dx='-10' dy='48'>W</text>
-                    <text text-anchor='middle' class='wday' dx='-10' dy='61' style='display: none;'>T</text>
-                    <text text-anchor='middle' class='wday' dx='-10' dy='74'>F</text>
-                    <text text-anchor='middle' class='wday' dx='-10' dy='87' style='display: none;'>S</text>
-                </g>
-            </svg>
+                            <text x='26' y='-5' class='month'>Aug</text>
+                            <text x='91' y='-5' class='month'>Sep</text>
+                            <text x='143' y='-5' class='month'>Oct</text>
+                            <text x='195' y='-5' class='month'>Nov</text>
+                            <text x='260' y='-5' class='month'>Dec</text>
+                            <text x='312' y='-5' class='month'>Jan</text>
+                            <text x='364' y='-5' class='month'>Feb</text>
+                            <text x='416' y='-5' class='month'>Mar</text>
+                            <text x='481' y='-5' class='month'>Apr</text>
+                            <text x='533' y='-5' class='month'>May</text>
+                            <text x='598' y='-5' class='month'>Jun</text>
+                            <text x='650' y='-5' class='month'>Jul</text>
+                            <text text-anchor='middle' class='wday' dx='-10' dy='9' style='display: none;'>S</text>
+                            <text text-anchor='middle' class='wday' dx='-10' dy='22'>M</text>
+                            <text text-anchor='middle' class='wday' dx='-10' dy='35' style='display: none;'>T</text>
+                            <text text-anchor='middle' class='wday' dx='-10' dy='48'>W</text>
+                            <text text-anchor='middle' class='wday' dx='-10' dy='61' style='display: none;'>T</text>
+                            <text text-anchor='middle' class='wday' dx='-10' dy='74'>F</text>
+                            <text text-anchor='middle' class='wday' dx='-10' dy='87' style='display: none;'>S</text>
+                        </g>
+                    </svg>
+                </td></tr>
+            </table>
             <br/>
             <div width='710pt' height='400pt' id='contributionsHolder' style='border:none'></div>";
         }
