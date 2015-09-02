59f92248781af6e42eddf6636f6c682f41cb4b65
ZanyGnu <ajaymartin@gmail.com>
2015-09-01 18:26:08 -0700

    simple ineffecient text similarity algo

--- RepoStats/Extensions.cs
+++ RepoStats/Extensions.cs
@@ -2,6 +2,7 @@
 namespace RepoStats
 {
     using System;
+    using System.Collections.Generic;
 
     static public class Extensions
     {
@@ -33,6 +34,19 @@ namespace RepoStats
         public static string EscapeForFormat(this string obj)
         {
             return obj.Replace("{0}", "%0%").Replace("{1}", "%1%").Replace("{", "{{").Replace("}", "}}").Replace("%0%", "{0}").Replace("%1%", "{1}");
+        }
+
+        public static bool AreSimilar(this string obj1, string obj2)
+        {
+            string[] s1s = obj1.Split(' ', '/', '\\', '\r', '\n', '.');
+            string[] s2s = obj2.Split(' ', '/', '\\', '\r', '\n', '.');
+            HashSet<string> s1Set = new HashSet<string>(s1s);
+            HashSet<string> s2Set = new HashSet<string>(s2s);
+            int count1 = s1Set.Count;
+            s1Set.IntersectWith(s2s);
+            int count2 = s1Set.Count;
+            double similarityIndex = count2 * 100.0 / count1;
+            return similarityIndex > 90;
         }
     }
 }
