9f1b95dffd589938f7e2aa487fc62e9ac115ef0e
ZanyGnu <ajaymartin@gmail.com>
2015-09-07 18:58:35 -0700

    bring online a sample owin api

--- RepoStats/Startup.cs
+++ RepoStats/Startup.cs
@@ -5,6 +5,8 @@ namespace RepoStats
     using Microsoft.Owin.FileSystems;
     using Microsoft.Owin.StaticFiles;
     using Owin;
+    using System;
+    using System.Web.Http;
 
     public class Startup
     {
@@ -18,32 +20,28 @@ namespace RepoStats
                 FileSystem = new PhysicalFileSystem(@".\"),
             });
 
-            //// Only serve files requested by name.
-            //app.UseStaticFiles("/commitsByAuthor");
-            ////app.UseStaticFiles("/commitsByDate");
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("Default", "{controller}/{checkinID}", new { controller = "Checkin", checkinID = RouteParameter.Optional });
 
-            //// Turns on static files, directory browsing, and default files.
-            //app.UseFileServer(new FileServerOptions()
-            //{
-            //    RequestPath = new PathString("/commitsByDate"),
-            //    EnableDirectoryBrowsing = true,
-            //});
+            config.Formatters.XmlFormatter.UseXmlSerializer = true;
+            config.Formatters.Remove(config.Formatters.JsonFormatter);
+            // config.Formatters.JsonFormatter.UseDataContractJsonSerializer = true;
 
-            //app.UseFileServer(new FileServerOptions()
-            //{
-            //    RequestPath = new PathString("/commitsByDate"),
-            //    FileSystem = new PhysicalFileSystem(@".\commitsByDate"),
-            //    EnableDirectoryBrowsing = true,
-            //});
+            app.UseWebApi(config);
+        }
+    }
 
-            //// Browse the root of your application (but do not serve the files).
-            //// NOTE: Avoid serving static files from the root of your application or bin folder,
-            //// it allows people to download your application binaries, config files, etc..
-            //app.UseDirectoryBrowser(new DirectoryBrowserOptions()
-            //{
-            //    RequestPath = new PathString("/src"),
-            //    FileSystem = new PhysicalFileSystem(@""),
-            //});
+    public class CheckinController : ApiController
+    {
+        public CheckinController()
+        {
+        }
+
+        // Gets
+        [HttpGet]
+        public String Get(string checkinId)
+        {
+            return "Hello world";
         }
     }
 }
