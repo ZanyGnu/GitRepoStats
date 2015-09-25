57b71186776a9e18b9bb30af40b0585065f01990
ZanyGnu <ajaymartin@gmail.com>
2015-09-24 20:37:55 -0700

    more refactor

--- RepoStats/Startup.cs
+++ RepoStats/Startup.cs
@@ -94,21 +94,7 @@ namespace RepoStats
         }
     }
 
-    public class RepositoryController : ApiController
-    {
-        [HttpGet]
-        public HttpResponseMessage Get()
-        {
-            string template = File.ReadAllText("views\\repository.cshtml");
-            string bodyContent = File.ReadAllText("RepoContributionMap.html");
-            var model = new { Title = "Repository Details", BodyContent = bodyContent };
-            string result = Engine.Razor.RunCompile(template, "templateName", null, model);
-            return new HttpResponseMessage()
-            {
-                Content = new StringContent(result, System.Text.Encoding.UTF8, "text/html"),
-            };
-        }
-    }
+
 
     public class UserController : ApiController
     {
