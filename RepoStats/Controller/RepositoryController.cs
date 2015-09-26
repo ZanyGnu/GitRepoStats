e668100a090168cf698274b115fa766dfd3685c8
ZanyGnu <ajaymartin@gmail.com>
2015-09-25 23:42:26 -0700

    repository controller

---
+++ RepoStats/Controller/RepositoryController.cs
@@ -0,0 +1,25 @@
+﻿
+namespace RepoStats.Controller
+{
+    using RazorEngine;
+    using RazorEngine.Templating;
+    using System.IO;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class RepositoryController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            string template = File.ReadAllText("views\\repository.cshtml");
+            string bodyContent = File.ReadAllText("RepoContributionMap.html");
+            var model = new { Title = "Repository Details", BodyContent = bodyContent };
+            string result = Engine.Razor.RunCompile(template, "templateName", null, model);
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(result, System.Text.Encoding.UTF8, "text/html"),
+            };
+        }
+    }
+}
