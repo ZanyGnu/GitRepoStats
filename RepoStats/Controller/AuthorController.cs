376f51c281097938f6035e2d3a482032694a3d08
ZanyGnu <ajaymartin@gmail.com>
2015-09-27 21:59:48 -0700

    author

---
+++ RepoStats/Controller/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿
+namespace RepoStats.Controller
+{
+    using RazorEngine;
+    using RazorEngine.Templating;
+    using System.IO;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class AuthorController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get(string user)
+        {
+            string template = File.ReadAllText("views\\user.cshtml");
+            string bodyContent = File.ReadAllText("CommitsByAuthor/" + user + "Contributions.html");
+            var model = new { Title = "Commit Details for " + user, BodyContent = bodyContent };
+            string result = Engine.Razor.RunCompile(template, "templateNameAuthor", null, model);
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(result, System.Text.Encoding.UTF8, "text/html"),
+            };
+        }
+    }
+}
